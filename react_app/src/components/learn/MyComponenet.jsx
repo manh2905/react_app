f7fb6d27a7c1562624776e12214c46c7a4f3f3be
Manh Vu <manhvu337@gmail.com>
2025-02-19 21:35:55 +0700

    vid30

--- react_app/src/components/learn/MyComponenet.jsx
+++ react_app/src/components/learn/MyComponenet.jsx
@@ -1,6 +1,17 @@
+import './style.css';
+
 
 const MyComponent = () => {
-    console.log(`hi`);
+    return (
+        <>
+            <div className="child"
+                style={
+                    { fontSize: "30px" }
+                }
+            >manh</div>
+
+        </>
+    );
 }
 
 export default MyComponent;
