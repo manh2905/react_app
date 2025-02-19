ec61548af69cd89c06ef7a6d9152f419803e85b9
Manh Vu <manhvu337@gmail.com>
2025-02-19 21:56:45 +0700

    vid31

--- react_app/src/components/learn/MyComponenet.jsx
+++ react_app/src/components/learn/MyComponenet.jsx
@@ -2,13 +2,19 @@ import './style.css';
 
 
 const MyComponent = () => {
+    // const a = "manh";
+    // const a = [1, 2, 3];
+    const a = {
+        name: "manh",
+        age: 25
+    }
+
     return (
         <>
             <div className="child"
-                style={
-                    { fontSize: "30px" }
-                }
-            >manh</div>
+                style={{ fontSize: "30px" }}
+            >{JSON.stringify(a)} & manh </div>
+            <div>{console.log("hello")}</div>
 
         </>
     );
