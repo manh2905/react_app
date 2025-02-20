ae0d40f2df4364cff614f66daf709bc2f9f9ae24
Manh Vu <manhvu337@gmail.com>
2025-02-20 22:34:44 +0700

    vid36

--- react_app/src/components/todo/TodoNew.jsx
+++ react_app/src/components/todo/TodoNew.jsx
@@ -2,10 +2,21 @@ const TodoNew = (props) => {
     const { addNewTodo } = props;
 
     // addNewTodo("manh");
+    const handleClick = () => {
+        alert("click me");
+    }
+    const handleOnchange = (name) => {
+        console.log("hihi", name);
+    }
     return (
         <div className='todo-add'>
-            <input type="text" />
-            <button>ADD</button>
+            <input type="text"
+                onChange={(event) => handleOnchange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >ADD</button>
         </div>
     );
 
