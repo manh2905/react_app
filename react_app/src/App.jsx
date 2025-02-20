bd6b33a614aaa37f38280d4fae3d35b537a66572
Manh Vu <manhvu337@gmail.com>
2025-02-20 21:54:18 +0700

    vid34

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -5,13 +5,23 @@ import Reactimage from './assets/react.svg'
 
 const App = () => {
 
+  const manh = "Manh";
+  const age = 25;
+  const data = {
+    addres: "Hanoi",
+    country: "VN"
+  }
 
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo list</div>
         <TodoNew />
-        <TodoData />
+        <TodoData
+          name={manh}
+          age={age}
+          data={data}
+        />
         <div className="todo-image">
           <img src={Reactimage} className='logo' />
         </div>
--- react_app/src/components/todo/TodoData.jsx
+++ react_app/src/components/todo/TodoData.jsx
@@ -1,6 +1,11 @@
-const TodoData = () => {
+const TodoData = (props) => {
+
+    console.log(props);
+    const { name, age, data } = props;
+    const { addres, country } = data;
     return (
         <div className='todo-tasks'>
+            <div>my name is {addres} </div>
             <div>learn react</div>
             <div>play game</div>
         </div>
