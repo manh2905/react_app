af3ff7376190a36a7af500521e9a1f45c08652e2
Manh Vu <manhvu337@gmail.com>
2025-02-20 21:14:23 +0700

    vid22

---
+++ react_app/src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className='todo-tasks'>
+            <div>learn react</div>
+            <div>play game</div>
+        </div>
+    );
+}
+
+export default TodoData;
---
+++ react_app/src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+const TodoNew = () => {
+    return (
+        <div className='todo-add'>
+            <input type="text" />
+            <button>ADD</button>
+        </div>
+    );
+
+}
+
+export default TodoNew;
